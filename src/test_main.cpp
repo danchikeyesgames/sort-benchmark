197ffeb43e0a20aba07bd5c9f4a5547d9c038f30
danchikeyesgames <danchikhol@gmail.com>
2025-03-30 00:55:29 +0300

    update test_main.c

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -8,8 +8,10 @@ int cmp(std::vector<int>::iterator& lv, std::vector<int>::iterator& rv) {
 
 int main() {
     std::vector<int> i;
+    std::vector<double> id;
 
     cpp_algosort_benchmark::SortAlgoContext<std::vector<int>::iterator> context;
+    cpp_algosort_benchmark::SortAlgoContext<std::vector<double>::iterator> context2;
     cpp_algosort_benchmark::CountingSort cs;
 
     i.push_back(200);
@@ -17,6 +19,13 @@ int main() {
 
     context.SetupAlgorithm(&cs);
     context(i.begin(), i.end(), cmp);
+
+
+    id.push_back(200.);
+    id.push_back(300.);
+
+    context2.SetupAlgorithm(&cs);
+    context2(id.begin(), id.end(), [](std::vector<double>::iterator& lv, std::vector<double>::iterator& rv) { return 0;});
 
     return 0;
 }
