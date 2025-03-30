57b03fbd0a3d8c2539e99955c2a89ff480337132
danchikeyesgames <danchikhol@gmail.com>
2025-03-30 18:53:31 +0300

    refac arch directories

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -1,6 +1,6 @@
-#include "counting_sort/CountingSort.hpp"
+#include "sort_algorithm/counting_sort.hpp"
 
-#include "SortAlgoContext.hpp"
+#include "sort_algo_context.hpp"
 
 int cmp(std::vector<int>::iterator& lv, std::vector<int>::iterator& rv) {
     return 0;
@@ -10,21 +10,21 @@ int main() {
     std::vector<int> i;
     std::vector<double> id;
 
-    cpp_algosort_benchmark::SortAlgoContext<std::vector<int>::iterator> context;
-    cpp_algosort_benchmark::SortAlgoContext<std::vector<double>::iterator> context2;
-    cpp_algosort_benchmark::CountingSort cs;
+    cpp_algosort_benchmark::sort_algorithm_context<std::vector<int>::iterator> context;
+    cpp_algosort_benchmark::sort_algorithm_context<std::vector<double>::iterator> context2;
+    cpp_algosort_benchmark::counting_sort cs;
 
     i.push_back(200);
     i.push_back(300);
 
-    context.SetupAlgorithm(&cs);
+    context.setup_algorithm(&cs);
     context(i.begin(), i.end(), cmp);
 
 
     id.push_back(200.);
     id.push_back(300.);
 
-    context2.SetupAlgorithm(&cs);
+    context2.setup_algorithm(&cs);
     context2(id.begin(), id.end(), [](std::vector<double>::iterator& lv, std::vector<double>::iterator& rv) { return 0;});
 
     return 0;
